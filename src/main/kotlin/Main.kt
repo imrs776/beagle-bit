a875e61beb1381acd8cd9cdc91503a93f1325b3c
Влад <imrs776@gmail.com>
2020-11-23 17:03:30 +0200

    oops

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package org.beagle
 
 fun main() {
-    val token = "NzE4Nzg5NTcyNzI4OTc5NTM5.Xtt-9A.kuIz6DvobcOVs1qjhHaNiD3WkbU"
+    val token = ""
     val prefix = "!"
     Bot(token, prefix)
             .addModule(PingPongModule())
